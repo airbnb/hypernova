d123054f0a82cb9f59c8cdaf0c1d3fcdcc9cb881
Josh Perez <josh.perez@airbnb.com>
2016-06-08 18:35:50 -0700

    2. Generating a new route
    Using `bin/rails generate controller welcome index` to generate a new
    route.

--- examples/simple/config/routes.rb
+++ examples/simple/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
