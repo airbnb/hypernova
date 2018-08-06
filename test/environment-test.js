50357b5f0a1b0ba91100e60d724aead83aa8fb11
Juan Carlos Medina <juanca.med@gmail.com>
2018-08-06 12:13:25 -0700

    Backfill environment tests for Promise
    It has a minimal Promise interface.

---
+++ test/environment-test.js
@@ -0,0 +1,24 @@
+import { assert } from 'chai';
+import '../src/environment';
+
+describe('environment', () => {
+  describe('global promise', () => {
+    it('has ES6 methods', () => {
+      assert.isFunction(global.Promise.prototype.then);
+      assert.isFunction(global.Promise.prototype.catch);
+      assert.isFunction(global.Promise.prototype.constructor);
+    });
+
+    it('has ES6 static methods', () => {
+      assert.isFunction(global.Promise.all);
+      assert.isFunction(global.Promise.race);
+      assert.isFunction(global.Promise.resolve);
+      assert.isFunction(global.Promise.reject);
+      assert.isFunction(global.Promise.cast);
+    });
+
+    it('removes extraneous methods', () => {
+      assert.isUndefined(global.Promise.promisifyAll);
+    });
+  });
+});
