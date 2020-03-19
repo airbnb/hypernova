9264345ae1e5137716939b79206fe35f36a6f423
barslev <benjaminbarslev@gmail.com>
2020-03-19 15:10:52 +0100

    Updates winston to version 3.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -16,9 +16,29 @@ const loggerInterface = {
     } else {
       const options = { ...OPTIONS, ...config };
 
-      logger = new winston.Logger({
+      logger = winston.createLogger({
         transports: [
-          new winston.transports.Console(options),
+          new winston.transports.Console((function(opts) {
+                const newOpts = {};
+                const formatArray = [];
+                const formatOptions = {
+                  stringify: () => winston.format((info) => { info.message = JSON.stringify(info.message); })(),
+                  formatter: () => winston.format((info) => { info.message = opts.formatter(Object.assign(info, opts)); })(),
+                  json: () => winston.format.json(),
+                  raw: () => winston.format.json(),
+                  label: () => winston.format.label(opts.label),
+                  logstash: () => winston.format.logstash(),
+                  prettyPrint: () => winston.format.prettyPrint({depth: opts.depth || 2}),
+                  colorize: () => winston.format.colorize({level: opts.colorize === true || opts.colorize === 'level', all: opts.colorize === 'all', message: opts.colorize === 'message'}),
+                  timestamp: () => winston.format.timestamp(),
+                  align: () => winston.format.align(),
+                  showLevel: () => winston.format((info) => { info.message = info.level + ': ' + info.message; })()
+                }
+                Object.keys(opts).filter(k => !formatOptions.hasOwnProperty(k)).forEach((k) => { newOpts[k] = opts[k]; });
+                Object.keys(opts).filter(k => formatOptions.hasOwnProperty(k) && formatOptions[k]).forEach(k => formatArray.push(formatOptions[k]()));
+                newOpts.format = winston.format.combine(...formatArray);
+                return newOpts;
+              })(options)),
         ],
       });
     }
